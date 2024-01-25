ec699bbac5527d2ba510224b55f33e1a30a6f450
Daniel Zautner <dzautner@gmail.com>
2024-01-25 16:44:20 +0200

    stupid test

--- app.py
+++ app.py
@@ -4,3 +4,15 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+#route that multiplies by 7
+@app.route('/multiply/<int:num>')
+def multiply(num):
+    return str(num*7)
+
+
+#route that says hello to a name
+@app.route('/hello/<name>')
+def hello(name):
+    return f"Hello, {name}!"
