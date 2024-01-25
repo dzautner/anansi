991b08e6cce7db68ff58bc957095956ac7de6c95
Daniel Zautner <dzautner@gmail.com>
2024-01-25 18:26:41 +0200

    add sms endpoint

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from twilio.twiml.messaging_response import MessagingResponse
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -53,6 +55,17 @@ def twilio_delivery_status():
 
     # Respond with a 200 OK status to acknowledge receipt
     return '', 200
+
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_reply():
+    """Respond to incoming calls with a simple text message."""
+    # Start our TwiML response
+    resp = MessagingResponse()
+
+    # Add a message
+    resp.message("The Robots are coming! Head for the hills!")
+
+    return str(resp)
 
 if __name__ == '__main__':
     app.run(debug=True)
